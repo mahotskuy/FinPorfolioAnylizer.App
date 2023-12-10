f19fa109cfe370b0681c861bbc76e7de747dc74e
Yurii-Mahotskyi-Yapi <yurii.mahotskiy@yapicentral.com>
2023-12-10 14:45:17 +0200

    Fix pages

--- Data/FinanceAnalyzer.cs
+++ Data/FinanceAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using FinPortfolioAnalyzer.Data.Models;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Schema;
 using Newtonsoft.Json.Schema.Generation;
